7ee654ebe641c8c635bb5cc9a0ce228d8ae09153
mfright <uj020288@ymail.com>
2014-05-11 05:10:39 +0900

    rev1
    rev1

---
+++ AutoYES1/AutoYES1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AutoYES1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timerAutoClick = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.lblStatus.Location = new System.Drawing.Point(-7, -1);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(165, 80);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "ダイアログ\r\n監視中";
+            // 
+            // timerAutoClick
+            // 
+            this.timerAutoClick.Enabled = true;
+            this.timerAutoClick.Interval = 1000;
+            this.timerAutoClick.Tick += new System.EventHandler(this.timerAutoClick_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 85);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(250, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(117, 17);
+            this.toolStripStatusLabel1.Text = "LEFT:     TOP:       ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(250, 107);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Outlook自動「はい」選択";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timerAutoClick;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ AutoYES1/AutoYES1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Diagnostics;
+
+namespace AutoYES1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        [DllImport("USER32.DLL")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+
+        int currentPointX, currentPointY; //現在のカーソルの座標
+        string messageKanshichu;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            messageKanshichu = lblStatus.Text;
+        }
+
+
+        private void timerAutoClick_Tick(object sender, EventArgs e)
+        {
+
+            // セキュリティダイアログの座標を取得
+            IntPtr dialog = FindWindow("#32770", "Microsoft Office Outlook");
+            Rect dialogRect = new Rect();
+            GetWindowRect(dialog, ref dialogRect);
+            toolStripStatusLabel1.Text = "LEFT:" + dialogRect.Left + "   TOP:" + dialogRect.Top + "   RIGHT:"+dialogRect.Right + "   BOTTOM:"+dialogRect.Bottom;
+
+            // 座標が0,0であれば、ダイアログは出ていないと考えられるので、処理を終了。
+            if (dialogRect.Right == 0 && dialogRect.Bottom == 0)
+            {
+                if (lblStatus.Text != messageKanshichu)
+                {
+                    lblStatus.Text = messageKanshichu;
+                }
+                return;
+            }
+
+            lblStatus.Text = "ダイアログ発見\r\nクリック中！";
+
+            //現在のカーソル位置を記憶しておく
+            //currentPointX = System.Windows.Forms.Cursor.Position.X;
+            //currentPointY = System.Windows.Forms.Cursor.Position.Y;
+
+            //カーソルを"はい"ボタン上まで移動させ、クリックする
+            setPoint(dialogRect.Left + 116, dialogRect.Top + 156);
+            doClick();
+
+        }
+
+
+        //クリックする処理
+        void doClick()
+        {
+            //[System.Runtime.InteropServices.DllImport("USER32.DLL")]
+            int MOUSEEVENTF_LEFTDOWN = 0x2;
+            int MOUSEEVENTF_LEFTUP = 0x4;
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        //カーソルを移動させる処理
+        void setPoint(int newX, int newY)
+        {
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(newX, newY);
+        }
+        
+    }
+}
